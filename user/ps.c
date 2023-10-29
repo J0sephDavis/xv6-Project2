4304dcb08eebddc1ef1835d2be70b8d09a80c9a1
Joseph Davis <josephdavisjoey@gmail.com>
2023-10-29 11:19:01 -0400

    likely finished the ps function. System continues on, but there is a persistent trap 14 err 5 with this prog (even when it was int main(){return0;})

--- user/ps.c
+++ user/ps.c
@@ -2,11 +2,22 @@
 #include "user.h"
 //this program will display the currently running processes on the system & some information about them
 int main() {
-//allocate table
-struct pstat *table = {0};
-getpinfo(table);
-printf(0, "%d", table->inuse[0]);
-
-//call getpinfo(table)
-//print results
+	//allocate table
+	struct pstat *table;
+	table = malloc(sizeof(struct pstat));
+	//load the table
+	getpinfo(table);
+	//print results
+	printf(1," used | pid  |tickets| ticks\n");
+	for (uint index = 0; index < NPROC; index++) {
+		if (table->inuse[index] == 1)
+			printf(0, "   %d  |   %d  |   %d   |  %d\n",
+				table->inuse[index],
+				table->pid[index],
+				table->tickets[index],
+				table->ticks[index]
+			);
+	}
+	//dellocate table
+	free(table);
 }
