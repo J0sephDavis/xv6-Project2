420e052db451e0c036c451b0e988a4ad9db6656e
Joseph Davis <josephdavisjoey@gmail.com>
2023-10-29 16:10:12 -0400

    updated ps.c, added comments, standardized main, added an exit()

--- user/ps.c
+++ user/ps.c
@@ -1,7 +1,8 @@
 #include "pstat.h"
 #include "user.h"
+
 //this program will display the currently running processes on the system & some information about them
-int main() {
+int main(int argc, char* argv[]) {
 	//allocate table
 	struct pstat *table;
 	table = malloc(sizeof(struct pstat));
@@ -9,15 +10,21 @@ int main() {
 	getpinfo(table);
 	//print results
 	printf(1," used | pid  |tickets| ticks\n");
-	for (uint index = 0; index < NPROC; index++) {
-		if (table->inuse[index] == 1)
-			printf(0, "   %d  |   %d  |   %d   |  %d\n",
+	for (uint index = 0; index < NPROC; index++) { 	//for-each process
+		int a = (table->inuse[index] 	!= 0); 	//if the program is in use
+		int b = (table->pid[index] 	!= 0); 	//if the pid is not 0
+		int c = (table->tickets[index] 	!= 0); 	//if the process has tickets
+		int d = (table->ticks[index] 	!= 0); 	//if the process has ever run
+		//
+		if (a || b || c || d)
+			printf(1, "   %d  |   %d  |   %d   |  %d\n",
 				table->inuse[index],
 				table->pid[index],
 				table->tickets[index],
 				table->ticks[index]
 			);
 	}
-	//dellocate table
+	//deallocate table
 	free(table);
+	exit();
 }
